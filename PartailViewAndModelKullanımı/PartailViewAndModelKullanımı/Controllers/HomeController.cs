eaca0d126971648aa3c40c9f2005d6bb2d515c46
TUGBAKARACAK1998 <tugbakaracak98@gmail.com>
2020-12-29 21:06:41 +0300

    first commit

---
+++ PartailViewAndModelKullanımı/PartailViewAndModelKullanımı/Controllers/HomeController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using PartailViewAndModelKullanımı.Models;
+
+namespace PartailViewAndModelKullanımı.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+
+        public ActionResult Login(Models.User veri)
+        { 
+
+        var data = new Models.User();
+            data.Ad = "Tuğba";
+            data.Soyad = "Karacak";
+            data.Yas = 22;
+
+
+            return View(data);
+        }
+     
+
+    
+           
+        }
+    }
+
---
+++ PartailViewAndModelKullanımı/PartailViewAndModelKullanımı/Models/User.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PartailViewAndModelKullanımı.Models
+{
+    public class User
+    {
+        public string Ad { get; set; }
+        public string Soyad { get; set; }
+        public int Yas { get; set; }
+    
+    }
+}
